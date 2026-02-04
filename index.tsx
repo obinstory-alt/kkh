45409dc24549626ad3b7a6260ac0bd5121b8707f
obinstory-alt <obinstory@gmail.com>
2026-02-05 08:56:48 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,15 +3,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-// v19.3 캐시 강제 갱신 및 최신 로직 반영
+// [v19.6 ENGINE] 브라우저 캐시 고착을 뚫기 위한 긴급 마운트 스크립트
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
 }
-
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
