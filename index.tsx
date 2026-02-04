a3780909f124c4fec937e7dd2fa416a6390360cf
obinstory-alt <obinstory@gmail.com>
2026-02-04 12:43:55 +0900

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// v18.1 캐시 강제 갱신용 주석 추가
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
